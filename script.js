c7d6e2ab0856bdffab323cd7b7b467c1bfb34399
Parvathi S Varma <parvathisvarma@gmail.com>
2024-09-05 19:09:21 +0530

    speed adjusting

--- script.js
+++ script.js
@@ -11,6 +11,5 @@ window.addEventListener('scroll',()=>{
     leaf.style.left = value*1.5 + 'px';
     hill5.style.left = value*1.5 + 'px';
     hill4.style.left = value*-1.5 + 'px';
-    hill4.style.top = value*1 + 'px';
-    hill1.style.top = value*1 + 'px';
+    hill1.style.top = value*-0.3 + 'px';
 })
